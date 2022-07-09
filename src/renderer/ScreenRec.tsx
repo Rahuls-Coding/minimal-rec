500015cb2f3784efc1eca1da9437c6a304a2d7e5
Rahul <rahulscodingworld@gmail.com>
2022-07-09 13:21:31 +0530

    finished ui for screen recorder

--- src/renderer/ScreenRec.tsx
+++ src/renderer/ScreenRec.tsx
@@ -1,19 +1,57 @@
 import {Button, useMantineTheme, Card} from '@mantine/core';
 import {DeviceDesktop} from 'tabler-icons-react';
+import { useState } from 'react';
+//@ts-ignore
+import RecordRTC, { RecordRTCPromisesHandler } from 'recordrtc'
 
 const ScreenRec = () => {
     const theme = useMantineTheme();
+    const BlinkingComponent = ({ highlighting }) => (
+        <div className={`element${highlighting ? " highlight" : ""}`}>◉</div>
+      );
+    const [highlight, setHighlight] = useState(false);
+    const [recorder, setRecorder] = useState<RecordRTC | null>()
+    const [stream, setStream] = useState<MediaStream | null>()
+    const [blob, setBlob] = useState<Blob | null>()
+    
+
+    const  startRecording = async () => {
+            setHighlight(true);
+            const mediaDevices = navigator.mediaDevices
+            const stream: MediaStream = await mediaDevices.getUserMedia({
+                audio: true,
+                video: true
+            })
+            const recorder = new RecordRTCPromisesHandler(stream, {
+                type: 'video',
+            }) 
+            await recorder.startRecording();
+      };
+
+    const stopRecording = async () => {
+        setHighlight(false);
+        await recorder.stopRecording();
+
+    }
+
     return(
         <div className="Total">
             <h1 className="icons"> {<DeviceDesktop size={32} color={theme.colors.blue[6]} className="icons-1" />}      Screen Recorder</h1>
             <Card color="gray" className="video-box"> 
             </Card>
             <div className="Area">
-                <Button className = "btn-1" color="blue" >Start</Button>
-                <div className="text">
+                <Button onClick={() => startRecording()}className = "btn-1" color="blue" >Start</Button>
+                <div className="record"> 
+                    <div className="text">
                     Current Status: 
+                    </div>
+                    <BlinkingComponent highlighting={highlight} />
+                    <div className="texty">
+                        {highlight ? "Recording" : "Not Recording"}
+                    </div>
                 </div>
-                <Button color="blue" className="btn-2">Stop</Button>
+               
+                <Button onClick = {() => stopRecording()}color="blue" className="btn-2">Stop</Button>
             </div>
         </div>
 
